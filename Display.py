9e6655fa320e40d935e2eac44fec0422dd624da0
querat_g <querat_g@epitech.eu>
2017-10-26 22:48:37 +0200

    implements basic stuff

--- Display.py
+++ Display.py
@@ -10,27 +10,30 @@ class Display(wx.Frame):
 
         self.bitmapPxWidth      = Config.NB_COLS * Config.SPRITE_SIZE
         self.bitmapPxHeight     = Config.NB_ROWS * Config.SPRITE_SIZE
-        # Bitmap holder to draw the game on
+
+        # Bitmap holder to draw the board on
         self.shownBoardBitmap   = wx.StaticBitmap(self, wx.ID_ANY)
         # Bitmap to draw on and render to the shown bitmap
-
         self.drawableBoardBitmap = wx.EmptyBitmap(self.bitmapPxWidth, self.bitmapPxHeight)
+
         self.shownBoardBitmap.SetBitmap(self.drawableBoardBitmap)
 
         # main window's scaling for it's internal components
         self.verticalSizer = wx.BoxSizer(wx.VERTICAL)
-
         self.doGuiLayout()
+
         self.Show()
 
+    # Fit everything visually
     def doGuiLayout(self):
         self.verticalSizer.Add(self.shownBoardBitmap, 1, wx.EXPAND | wx.ALL)
-        self.SetSizer(self.verticalSizer)
+        self.SetSizerAndFit(self.verticalSizer)
 
     def drawBoard(self, board):
         for row in board.getMatrix():
             for cell in row:
                 self.drawCell(cell)
+        # Forces redrawing
         self.shownBoardBitmap.SetBitmap(self.drawableBoardBitmap)
 
     def drawCell(self, cell):
