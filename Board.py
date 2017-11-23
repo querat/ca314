b4bf01c5b09300424552e3eaad2d3e9dc4459382
David BANGET-MOSSAZ <dbangetmossaz@gmail.com>
2017-11-23 11:53:05 +0000

    start resolve

--- Board.py
+++ Board.py
@@ -18,3 +18,33 @@ class Board:
 
     def getMatrix(self):
         return self.matrix
+
+    def getSimpleArray(self):
+        simpleArray = []
+        for row in self.matrix:
+            simpleRow = []
+            for cell in row:
+                simpleRow.append(cell.status)
+            simpleArray.append(simpleRow)
+        return simpleArray;
+
+    def replace(self, array, val1 , val2):
+        for row in array:
+            for cell in row:
+                if (cell == val1 || cell == val2):
+                    cell = (cell == val1) ? (val2) : (val1)
+
+    def calculScore(self):
+        blackSimpleArray = self.getSimpleArray()
+        whiteSimpleArray = self.replace(list(blackSimpleArray), Cell.Status.BLACK, Cell.Status.WHITE)
+
+        blackNpArray = np.array(blackSimpleArray).astype(float)
+        whiteNpArray = np.array(whiteSimpleArray).astype(float)
+
+        resultBlackArray = fill.fast_fill(blackNpArray, four_way=True)
+        resultWhiteArray = fill.fast_fill(whiteNpArray, four_way=True)
+
+        print("Results:\n");
+        print(resultBlackArray)
+        print("")
+        print(resultWhiteArray);
