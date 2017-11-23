048939ac046b54b8df2c3cce32c118d2a647e868
David BANGET-MOSSAZ <dbangetmossaz@gmail.com>
2017-11-23 11:30:53 +0000

    Referee

---
+++ Referee.py
@@ -0,0 +1,12 @@
+from    Board  import Board
+from    Cell    import Cell
+
+class Referee:
+
+    def __init__(self, board):
+        self.board = board
+
+    def checkMove(self, x, y, player):
+
+    def isGameOver(self):
+        return False;
